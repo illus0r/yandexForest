04050392a524cb1d571cca3308c519083e03c549
illus0r <zor667@gmail.com>
2024-08-29 17:14:13 +0300

    fix

--- index.js
+++ index.js
@@ -1,59 +1,24 @@
-// let sketches = []
-
-// function preload() {
-//   // Загрузка всех скетчей
-//   for (let i = 1; i <= 100; i++) {
-//     loadScript(`programmer${i}/sketch.js`)
-//   }
-// }
-
-// function setup() {
-//   createCanvas(windowWidth, windowHeight)
-
-//   // Инициализация всех скетчей
-//   for (let i = 0; i <= 3; i++) {
-//     if (window[`programmer${i}Sketch`]) {
-//       let sketchInstance = new p5(window[`programmer${i}Sketch`])
-//       sketches.push(sketchInstance)
-
-//       // Вызываем setup для каждого скетча
-//       if (sketchInstance.setup) {
-//         sketchInstance.setup()
-//       }
-//     }
-//   }
-// }
-
-// function draw() {
-//   background(20)
-
-//   // Обновление и отрисовка всех скетчей
-//   for (let sketch of sketches) {
-//     console.log('sketch:', sketch)
-//     if (sketch.draw) {
-//       push()
-//       sketch.draw()
-//       pop()
-//     }
-//   }
-// }
-
-// // Вспомогательная функция для загрузки скриптов
-// function loadScript(url) {
-//   return new Promise((resolve, reject) => {
-//     let script = document.createElement('script')
-//     script.src = url
-//     script.onload = resolve
-//     script.onerror = reject
-//     document.head.appendChild(script)
-//   })
-// }
-
-// window.addEventListener('load', () => {
 const body = document.querySelector('body')
 
-const container1 = document.createElement('div')
-container1.id = 'sketch1-container'
-body.appendChild(container1)
-debugger
-// })
+for (let i = 1; i <= 2; i++) {
+  const container = document.createElement('div')
+  container.id = `sketch${i}-container`
+  document.body.appendChild(container)
+
+  // Создаем новый экземпляр p5 и привязываем его к текущему контейнеру
+  new p5(p => {
+    p.setup = function () {
+      let canvas = p.createCanvas(200, 400)
+      canvas.parent(`sketch${i}-container`)
+      p.x = p.random(p.width)
+      p.y = p.random(p.height)
+    }
+
+    p.draw = function () {
+      p.fill(255, 0, 0)
+      p.ellipse(p.x, p.y, 50, 20)
+      p.x = (p.x + 1) % p.width
+      p.y = (p.y + 1) % p.height
+    }
+  })
+}
--- programmer2/sketch.js
+++ programmer2/sketch.js
@@ -7,7 +7,7 @@ function setup() {
 }
 
 function draw() {
-  fill(25, 230, 0)
+  fill(255, 0, 0)
   ellipse(x, y, 50, 50)
   x = (x + 1) % width
   y = (y + 1) % height
