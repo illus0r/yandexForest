5a8c9a28f086cd13bcda8486041dfac2461663fd
illus0r <zor667@gmail.com>
2024-09-02 14:55:09 +0300

    changing iframes

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const body = document.querySelector('body')
-
-for (let i = 1; i <= 2; i++) {
-  const container = document.createElement('div')
-  container.id = `sketch${i}-container`
-  document.body.appendChild(container)
-
-  // Создаем новый экземпляр p5 и привязываем его к текущему контейнеру
-  new p5(p => {
-    p.setup = function () {
-      let canvas = p.createCanvas(200, 400)
-      canvas.parent(`sketch${i}-container`)
-      p.x = p.random(p.width)
-      p.y = p.random(p.height)
-    }
-
-    p.draw = function () {
-      p.fill(255, 0, 0)
-      p.ellipse(p.x, p.y, 50, 20)
-      p.x = (p.x + 1) % p.width
-      p.y = (p.y + 1) % p.height
-    }
+let iframes = []
+let srcs = [
+  'programmer1/index.html',
+  'programmer2/index.html',
+  'programmer3/index.html',
+]
+function createIframe(src) {
+  var iframe = document.createElement('iframe')
+  iframe.src = src
+  return iframe
+}
+window.onload = function () {
+  srcs.forEach(function (src) {
+    let fr = createIframe(src)
+    fr.setAttribute('width', '100%')
+    fr.setAttribute('height', '1000px')
+    iframes.push(fr)
   })
+  animate()
+}
+
+let current = 0
+function animate() {
+  //remove all iframes
+  document.querySelectorAll('iframe').forEach(function (iframe) {
+    iframe.parentNode.removeChild(iframe)
+  })
+  current = (current + 1) % iframes.length
+  document.body.appendChild(iframes[current])
+  setTimeout(animate, 1000)
 }
